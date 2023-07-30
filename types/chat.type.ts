6df6b3c58341ecf3ee839f6f9739441685ba3966
eggplantiny <eggplantiny@gmail.com>
2023-07-30 17:23:44 +0900

    asdasd

--- types/chat.type.ts
+++ types/chat.type.ts
@@ -1,6 +1,6 @@
-type MBTI = 'ISFJ' | 'ISFP' | 'ISTJ' | 'ISTP' | 'INFJ' | 'INFP' | 'INTJ' | 'INTP' | 'ESFJ' | 'ESFP' | 'ESTJ' | 'ESTP' | 'ENFJ' | 'ENFP' | 'ENTJ' | 'ENTP'
-export interface MbtiTemplate {
-  [key: MBTI]: {
-    description: string[]
-  }
-}
+export type MBTI = 'ISFJ' | 'ISFP' | 'ISTJ' | 'ISTP' | 'INFJ' | 'INFP' | 'INTJ' | 'INTP' | 'ESFJ' | 'ESFP' | 'ESTJ' | 'ESTP' | 'ENFJ' | 'ENFP' | 'ENTJ' | 'ENTP'
+
+export type MbtiTemplate = Record<MBTI, {
+  descriptions: string[]
+  avatar: any
+}>

c6511b511cf4263d303301f78aa8d3ebd2c98155
10148120 <sh827kim@gmail.com>
2023-07-30 16:42:40 +0900

    작업물 일단 커밋

--- server/api/aimigos/index.get.ts
+++ server/api/aimigos/index.get.ts
@@ -1,11 +1,13 @@
 import { getServerSession } from '#auth'
 import { getAimigos } from '~/server/data/aimigos'
+import { getUserByEmail } from '~/server/data/users'
 import { getListSuccessResponse } from '~/server/utils/CommonResult'
 
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event)
   const email = session.user?.email ?? ''
 
+  const user = await getUserByEmail(email)
   const aimigos = await getAimigos(userId)
   return getListSuccessResponse(aimigos)
 })

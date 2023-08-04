5ddb9565f821365e36faa65006401e960ada93ac
eggplantiny <eggplantiny@gmail.com>
2023-08-04 17:39:09 +0900

    세션리스 스타일 수정

--- layouts/sessionless.vue
+++ layouts/sessionless.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Footer from '~/components/organisms/Footer.vue'
 </script>
 
 <template>
@@ -7,13 +6,11 @@ import Footer from '~/components/organisms/Footer.vue'
     <main>
       <NuxtPage class="h-full" />
     </main>
-    <Footer />
   </div>
 </template>
 
 <style scoped lang="scss">
 main {
   @apply container mx-auto max-w-3xl md:max-w-6xl px-6 mt-2 relative h-full;
-  min-height: calc(100vh + 64px);
 }
 </style>

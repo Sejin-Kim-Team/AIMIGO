265d9e5187e74be592bc19fb7a13dc1b7e995143
eggplantiny <eggplantiny@gmail.com>
2023-08-04 18:28:44 +0900

    _content 허용

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,6 +1,9 @@
 import { getServerSession } from '#auth'
 
 export default eventHandler(async (event) => {
+  if (event.path.includes('_content'))
+    return
+
   const session = await getServerSession(event)
 
   const uri = getRequestURL(event).pathname

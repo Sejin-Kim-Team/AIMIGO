a9f51be719198cca3ee4a3ece2cf08f79cc6ff03
Sejin Kim <lemon05090@gmail.com>
2023-08-05 00:29:01 +0900

    프롬프트 템플릿 수정

--- utils/dates.ts
+++
@@ -1,6 +0,0 @@
-import dayjs from 'dayjs'
-import type { Dayjs } from 'dayjs'
-
-export function formatDateString(date: string | Date | Dayjs, format = 'YYYY-MM-DD HH:mm:ss') {
-  return dayjs(date).format(format)
-}
